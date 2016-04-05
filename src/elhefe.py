33b012a61df2d21e7cc66838041cbe02339293bd
Don Johnson <dj@codetestcode.io>
2016-04-05 15:20:21 -0700

    Added clear log

--- src/elhefe.py
+++ src/elhefe.py
@@ -22,23 +22,26 @@ def get_rand_urls(fn,urlnum):
     with open(fn, "r") as f:
         list2 = []
         for item in f:
-            list2.append(item)
+            list2.append(item.rstrip('\n'))
 
         return random.sample(list2,urlnum)
 
 
 def get_url_safari(fn, urlnum):
     urllst = get_rand_urls(fn,urlnum)
-
+    print urllst
     for url in urllst:
-        print 'Accessing: {}'.format(url)
-        print '='*int(len(url))
-        print get_timestamp()
-        webbrowser.get('safari').open_new_tab(url)
+        time.sleep(random.randint(1, 8))
+        webbrowser.get('safari').open_new(url)
+        time.sleep(10)
+        with open('interactions.log','a+') as logfile:
+            logfile.write('{}, {}\n'.format(url, get_timestamp()))
+
     close_app('Safari')
 
 
 
 
-
-get_url_safari('data/validUrls.txt',100)
+for x in range(10):
+    time.sleep(5)
+    get_url_safari('data/validUrls.txt',1)
